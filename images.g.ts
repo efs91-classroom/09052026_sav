6c4c361fb19a8eced25237cf876085b132a2fcde
efs91-classroom <benoit@efs91.fr>
2026-06-13 09:24:30 +0200

    add ES sprinte

---
+++ images.g.ts
@@ -0,0 +1,51 @@
+// Code généré automatiquement. Ne pas modifier.
+namespace myImages {
+
+    helpers._registerFactory("image", function(name: string) {
+        switch(helpers.stringTrim(name)) {
+            case "image1":
+            case "myImage":return img`
+. . . . . . . . . . . . . . . . 
+. . . . . . . . . . . . . . . . 
+. . . . . . . . . . . . . . . . 
+. . . . . . . . . . . . . . . . 
+. . . 3 3 3 3 3 . 3 3 3 3 3 . . 
+. . . 3 . . . . . 3 3 . . . . . 
+. . . 3 . . . . . . 3 . . . . . 
+. . . 3 3 3 . . . 3 3 3 3 3 . . 
+. . . 3 . . . . . . . . 3 . . . 
+. . . 3 . . . . . . . . 3 3 . . 
+. . . 3 3 3 3 3 . 3 3 3 3 3 . . 
+. . . . . . . . . 3 . . . . . . 
+. . . . . . . . . . . . . . . . 
+. . . . . . . . . . . . . . . . 
+. . . . . . . . . . . . . . . . 
+. . . . . . . . . . . . . . . . 
+`;
+        }
+        return null;
+    })
+
+    helpers._registerFactory("animation", function(name: string) {
+        switch(helpers.stringTrim(name)) {
+
+        }
+        return null;
+    })
+
+    helpers._registerFactory("song", function(name: string) {
+        switch(helpers.stringTrim(name)) {
+
+        }
+        return null;
+    })
+
+    helpers._registerFactory("json", function(name: string) {
+        switch(helpers.stringTrim(name)) {
+
+        }
+        return null;
+    })
+
+}
+// Code généré automatiquement. Ne pas modifier.
